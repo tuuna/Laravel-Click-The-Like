8604d4b9443a2e3ce8d78d7b99ca52f16c879284
赖杰 <vampirebitter@users.noreply.github.com>
2016-12-19 16:04:23 +0800

    Update IndexController.php

--- click/app/Http/Controllers/IndexController.php
+++ click/app/Http/Controllers/IndexController.php
@@ -6,7 +6,22 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    public function index() {
-        return view('index');
+    public function index() 
+    {
+    	$allClick = Redis:get(" click : like ");
+
+        return view('index',['click' => $allClick]);
+    }
+
+    public function getAllData()
+    {
+    	$data = Request::post();
+
+    	if($data['count']) {
+    		$key = 'click : like';
+    		$allKey = Redis:get($key);
+    		Redis:set($key,$allKey++);
+    	}
     }
 }
+
